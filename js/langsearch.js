4b95fd50fcafc723facf018b18d2fbf8e559e09e
harshkothari410 <harshkothari410@gmail.com>
2013-08-15 00:14:05 +0530

    added data of autonym and other detail

--- js/langsearch.js
+++ js/langsearch.js
@@ -26,7 +26,6 @@ $(function () {
 				},
 				success: function(data, textStatus, xhr) {
 					//called when successful
-					//$('#input').val(data[0]).addClass("muted");
 					process(data);
 				},
 				error: function(xhr, textStatus, errorThrown) {
--- script/nametoautonym.php
+++ script/nametoautonym.php
@@ -6,11 +6,20 @@
 	$dbs = mysql_select_db($databaseName,$con);
 	mysql_query("SET NAMES 'utf8'", $con);
 
-	$query = 'select langname_eng, langname_autonym from langdetail';
+	$query = 'select langname_eng, langname_autonym, langcode_iso from langdetail';
 	$result = mysql_query($query,$con);
-
+	$i = 0;
 	while ($result1 = mysql_fetch_assoc($result)) {
-		$array[$result1['langname_eng']] = utf8_decode( utf8_encode($result1['langname_autonym']));
+		//$array[$result1['langname_eng']] =  utf8_decode( utf8_encode($result1['langname_autonym']));
+		#$array[] = $result1['langname_eng'] . ' - ' .utf8_decode( utf8_encode($result1['langname_autonym']));
+		$array[$i]['autonym'] = utf8_decode( utf8_encode($result1['langname_autonym']));
+		$array[$i]['name'] = $result1['langname_eng'];
+		$array[$i]['value'] = $result1['langname_eng'];
+		$array[$i]['iso'] = $result1['langcode_iso'];
+		$array[$i]['tokens'][] = $result1['langname_eng'];
+		$array[$i]['tokens'][] = $result1['langcode_iso'];
+		$array[$i]['tokens'][] = utf8_decode( utf8_encode($result1['langname_autonym']));
+		$i = $i + 1;
 	}
 
 	$jsondata = json_encode($array);
