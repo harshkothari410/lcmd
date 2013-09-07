caac49238d98aa6bce7aed5c63a256521ae921c5
harshkothari410 <harshkothari410@gmail.com>
2013-09-08 03:52:15 +0530

    added .py file for  fetching iso and eng name of languages

--- script/isolist.py
+++ script/isolist.py
@@ -1,6 +1,29 @@
 # -*- coding: utf-8 -*-
 import requests,json
 
+def dicttojson(dictdata):
+	return "harsh"
+
+
 s = requests.get('http://www.loc.gov/standards/iso639-2/php/code_list.php')
 
-print s.text
+data = s.text
+
+s = '<td scope="row">'
+datadict = {}
+datalist = []
+
+while data.find(s) != -1:
+	iso = data[data.find(s )+ len(s) : data.find(s )+ len(s) + 3]
+	td = '<td>'
+	temp = data[data.find(s)+ len(s) :data.find(s )+ len(s) + 100]
+	temp = temp[temp.find(td) + len(td):]
+	temp = temp[temp.find(td) + len(td):]
+	engname = temp[:temp.find('</td>')]
+	data = data[data.find(s)+ len(s) :]
+	datadict[iso] = engname
+	print [iso,engname]
+	datalist.append([iso,engname])
+
+#print datadict
+#print datalist
