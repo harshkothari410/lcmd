4a81ad8e4883321d407dfcb2c7d4655db97129a8
harshkothari410 <harshkothari410@gmail.com>
2013-09-11 02:54:36 +0530

    done some fixes in script

--- script/isolist.py
+++ script/isolist.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 import requests,json
 
-def dicttojson(dictdata):
-	return "harsh"
-
-
 s = requests.get('http://www.loc.gov/standards/iso639-2/php/code_list.php')
 
 data = s.text
@@ -14,16 +10,22 @@ datadict = {}
 datalist = []
 
 while data.find(s) != -1:
-	iso = data[data.find(s )+ len(s) : data.find(s )+ len(s) + 3]
+	iso = data[data.find(s) + len(s) : data.find(s) + len(s) + 3]
 	td = '<td>'
-	temp = data[data.find(s)+ len(s) :data.find(s )+ len(s) + 100]
+	temp = data[data.find(s) + len(s) : data.find(s) + len(s) + 100]
 	temp = temp[temp.find(td) + len(td):]
 	temp = temp[temp.find(td) + len(td):]
 	engname = temp[:temp.find('</td>')]
 	data = data[data.find(s)+ len(s) :]
 	datadict[iso] = engname
-	datalist.append([iso,engname])
+	datalist.append(iso)
 
-json = json.dumps(datadict,ensure_ascii=False, indent=4, separators=(',', ': '))
+data = json.dumps(datalist,ensure_ascii = False, indent = 4, separators = (',', ': '))
+
+json = json.dumps(datadict,ensure_ascii = False, indent = 4, separators = (',', ': '))
+
 f = open('../data/isolist.json','w')
 f.write(json)
+
+f1 = open('../data/isodropdown.json','w')
+f1.write(data)
